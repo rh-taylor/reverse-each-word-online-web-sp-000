d8a6a1305cdc55d9f1bd863b00277d0201a34c89
Robyn Taylor <rhtaylor0715@gmail.com>
2021-04-05 21:42:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,8 @@
 #reverse_each_word using each
 def reverse_each_word(string)
-  array_to_string = []
-  string_to_array = string.split(" ")
-  string_to_array.each do |index|
-    array_to_string << index.reverse
+  reversed_string = string.split(" ")
+  reversed_string.collect do |word|
+    word.reverse
   end
+  reversed_string.join
 end
-
-
-
